bb64a328147c5f895ba5c5ff4ee0d83e8a83221a
Tristan Godfrey <tristan@godfrey.nl>
2021-08-24 20:03:13 +0200

    Delete old server

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -11,7 +11,7 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-  
+
   @Get("auth/spotify/callback")
   @UseGuards(AuthGuard('spotify'))
   spotifyCallback(): string {
